f5b461bfbee0ee6288837760237cad1206b9e319
zRakan <placeholder@this.gg>
2024-12-17 16:47:59 +0300

    feat: Redirect non-authenticated user to `/login`

---
+++ middleware/auth.global.js
@@ -0,0 +1,11 @@
+export default defineNuxtRouteMiddleware(async function(to, from) {
+    if(!to.name) return navigateTo('/');
+
+    const { loggedIn, fetch } = useUserSession();
+    await fetch();
+
+    const isLogged = loggedIn.value;
+    
+    if(!isLogged && to.path != '/login') return navigateTo('/login');
+    else if(isLogged && to.path == '/login') return navigateTo('/');
+});
