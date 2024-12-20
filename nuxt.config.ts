8dc6c269142caf4848b997bcecbfe1e084c62f0e
zRakan <placeholder@this.gg>
2024-12-21 00:36:31 +0300

    style: Global `main.css`

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,4 +3,5 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   modules: ['nuxt-auth-utils', '@nuxt/ui', 'nuxt-bootstrap-icons'],
+  css: ['~/assets/main.css']
 })
