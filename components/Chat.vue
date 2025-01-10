97fe6b7d592de1ec3183588b09c9f7f2f6ea97c9
zRakan <placeholder@this.gg>
2025-01-10 14:23:18 +0300

    feat: Loading animation when sending a message

--- components/Chat.vue
+++ components/Chat.vue
@@ -1,12 +1,22 @@
 <script setup>
     import { VueShowdown } from 'vue-showdown';
 
+    const states = ref({
+        pending: false
+    })
+
     const messages = useState('chat:history', () => { return [] });
     const selectedChat = useState('chat:selected', () => { return null });
     const chatList = useState('chat:list', () => { return null })
     
     const message = ref('');
-    async function send({ channelId, sentMessage, intro = false }) {        
+    async function send({ channelId, sentMessage, intro = false }) {    
+        if(states.value.pending) return;
+
+        if(sentMessage)
+            message.value = sentMessage;
+        
+        states.value.pending = true;
         const data = await $fetch('/api/send', {
             method: "POST",
 
@@ -18,6 +28,8 @@
             chatList.value.push({ id: data.chat.id, title: data.chat.title });
             await selectChat(data.chat.id);
         }
+
+        states.value.pending = false;
 
         // Reset input
         message.value = '';
@@ -49,19 +61,25 @@
         <p class="text-3xl">Welcome to Mostaelim</p>
 
         <div id="input-container" class="h-[50px] w-[500px] my-2 flex items-center rounded-3xl bg-white dark:bg-[#1e1e1e] p-1 shadow-2xl dark:shadow-none">
-            <input @keyup.enter="send({ intro: true })" v-model="message" placeholder="Enter your message..." class="h-full w-full rounded-3xl p-2 border-none outline-none text-sm dark:bg-[#1e1e1e]" />
-            <UIcon @click="send({ intro: true })" class="w-6 h-6 cursor-pointer hover:text-primary-500 transition-colors" name="material-symbols:send-outline-rounded" />
+            <input @keyup.enter="send({ intro: true })" v-model="message" placeholder="Enter your message..." class="h-full w-full rounded-3xl p-2 border-none outline-none text-sm dark:bg-[#1e1e1e]" :disabled="states.pending" />
+            <UIcon @click="send({ intro: true })" class="w-6 h-6 cursor-pointer aria-disabled:cursor-not-allowed hover:text-primary-500 transition-colors" :name="!states.pending ? 'material-symbols:send-outline-rounded' : 'material-symbols:stop-circle'" :aria-disabled="states.pending" />
         </div>
 
         <div class="grid grid-cols-2 gap-2 w-[470px]">
-            <UButton block size="xs" v-for="suggest in suggestions" @click=suggestion(suggest) :label="suggest" />
+            <UButton block size="xs" v-for="suggest in suggestions" @click=suggestion(suggest) :label="suggest" :disabled="states.pending" />
+        </div>
+
+        <div v-if="states.pending" class="lds-ellipsis">
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
         </div>
     </div>
 
     <!-- If chat is chosen -->
     <div v-else id="chat-container" class="w-[calc(80%-6rem)] flex flex-col items-center">
         <div id="chat-content" class="w-full h-full py-8 pl-24 overflow-y-auto">
-
             <ClientOnly>
                 <template v-for="(message, index) in messages">
                     <UDivider class="p-5" v-if="message.role == 'user' && index > 0 && messages[index-1].role == 'assistant'" />
@@ -89,9 +107,16 @@
             </ClientOnly>
         </div>
 
+        <div v-if="states.pending" class="lds-ellipsis bottom-10">
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+
         <div id="input-container" class="h-[50px] w-[500px] flex items-center mb-5 mt-auto rounded-3xl bg-white dark:bg-[#1e1e1e] p-1 shadow-2xl dark:shadow-none">
             <input @keyup.enter="send" v-model="message" placeholder="Enter your message..." class="h-full w-full rounded-3xl p-2 border-none outline-none text-sm dark:bg-[#1e1e1e]" />
-            <UIcon @click="send" class="w-6 h-6 cursor-pointer hover:text-primary-500 transition-colors" name="material-symbols:send-outline-rounded" />
+            <UIcon @click="send" class="w-6 h-6 cursor-pointer aria-disabled:cursor-not-allowed hover:text-primary-500 transition-colors" :name="!states.pending ? 'material-symbols:send-outline-rounded' : 'material-symbols:stop-circle'" :aria-disabled="states.pending" />
         </div>
     </div>
 </template>
